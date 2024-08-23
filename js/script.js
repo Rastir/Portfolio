2d26564b7bf9af2f842bb252862fd3eb1aa13d06
Rastir <aldojero@hotmail.com>
2024-08-23 15:52:53 -0500

    octavo commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 $(document).ready(function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.carousel-item');
 
-	// Hero Section FUll Height
-	$(function () {
-		var windowH = $(window).height();
-		var wrapperH = $('.hero').height();
-		if (windowH > wrapperH) {
-			$('.hero').css({ 'height': ($(window).height()) + 'px' });
-		}
-		$(window).resize(function () {
-			var windowH = $(window).height();
-			var wrapperH = $('.hero').height();
-			var differenceH = windowH - wrapperH;
-			var newH = wrapperH + differenceH;
-			var truecontentH = $('#truecontent').height();
-			if (windowH > truecontentH) {
-				$('.hero').css('height', (newH) + 'px');
-			}
+    function showSlide(index) {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (index + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
 
-		})
-	});
+    function nextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
+
+    // Hero Section Full Height
+    $(function () {
+        var windowH = $(window).height();
+        var wrapperH = $('.hero').height();
+        if (windowH > wrapperH) {
+            $('.hero').css({ 'height': ($(window).height()) + 'px' });
+        }
+        $(window).resize(function () {
+            var windowH = $(window).height();
+            var wrapperH = $('.hero').height();
+            var differenceH = windowH - wrapperH;
+            var newH = wrapperH + differenceH;
+            var truecontentH = $('#truecontent').height();
+            if (windowH > truecontentH) {
+                $('.hero').css('height', (newH) + 'px');
+            }
+        });
+    });
 });
